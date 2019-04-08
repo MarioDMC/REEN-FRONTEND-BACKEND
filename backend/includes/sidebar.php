25fa51188c3ef265a06986a87f7677061399628f
MarioDMC <MarioDMC@users.noreply.github.com>
2019-04-07 19:15:16 -0500

    Añado Dashboard de Backend

---
+++ backend/includes/sidebar.php
@@ -0,0 +1,25 @@
+<!-- Left Sidebar -->
+	<div class="left main-sidebar">
+	
+		<div class="sidebar-inner leftscroll">
+
+			<div id="sidebar-menu">
+        
+			<ul>
+
+					<li>
+						<a class="active" href="index.php"><i class="fa fa-fw fa-bars"></i><span> Dashboard </span> </a>
+                    </li>
+				
+            </ul>
+
+            <div class="clearfix"></div>
+
+			</div>
+        
+			<div class="clearfix"></div>
+
+		</div>
+
+	</div>
+<!-- End Sidebar -->
